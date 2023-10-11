563dec7a22f49cfdaecffd7b1e75af7c57444998
Ivan Evchatov <ievchatov@inbox.ru>
2023-10-11 23:40:06 +0700

    Added databases context

--- Data/AppDbContext.cs
+++ Data/AppDbContext.cs
@@ -1,1 +1,31 @@
-﻿
+﻿using Microsoft.EntityFrameworkCore;
+using food_delivery.Data.Models;
+
+namespace food_delivery.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        public DbSet<AddressElement> AddressElements { get; set; }
+
+        public DbSet<Dish> Dishes { get; set; }
+
+        public DbSet<DishInCart> DishesInCart { get; set; }
+
+        public DbSet<Hirerarhy> Hirerarhy { get; set; }
+
+        public DbSet<House> Houses { get; set; }
+
+        public DbSet<Order> Orders { get; set; }
+
+        public DbSet<Rating> Ratings { get; set; }
+
+        public DbSet<User> Users { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
