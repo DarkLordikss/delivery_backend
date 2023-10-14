ff7ea6dceb95ba2b1a90c06bb390218eb32a3f57
Ivan Evchatov <ievchatov@inbox.ru>
2023-10-14 11:18:19 +0700

    Added Basket model

---
+++ Data/Models/Basket.cs
@@ -0,0 +1,14 @@
+﻿namespace food_delivery.Data.Models
+{
+    public class Basket
+    {
+        public List<BasketItem> Items { get; set; }
+        public BasketTotal Total { get; set; }
+
+        public Basket()
+        {
+            Items = new List<BasketItem>();
+            Total = new BasketTotal();
+        }
+    }
+}
