c2aa77ed48b09f8501ace94ac3d4f7f4f13a41e0
Ivan Evchatov <ievchatov@inbox.ru>
2023-10-15 23:03:55 +0700

    Added Salt to Password class

---
+++ Services/UserService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Text;
+using food_delivery.Data;
+using food_delivery.Data.Models;
+
+namespace food_delivery.Services
+{
+    public class UserService
+    {
+        private readonly AppDbContext _context;
+
+        public UserService(AppDbContext context)
+        {
+            _context = context;
+        }
+    }
+}
