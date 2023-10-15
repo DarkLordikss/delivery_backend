4e2bd595da5e8fee49c545053686febe4adaa1f9
Ivan Evchatov <ievchatov@inbox.ru>
2023-10-15 23:46:10 +0700

    Changed User format for right db format

--- Data/Models/User.cs
+++ Data/Models/User.cs
@@ -1,11 +1,15 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
+using System.Runtime.Serialization;
 
 namespace food_delivery.Data.Models
 {
     public class User
     {
         [Key]
+        [IgnoreDataMember]
+        [DefaultValue(typeof(Guid), "00000000-0000-0000-0000-000000000000")]
         public Guid Id { get; set; }
 
         public string FullName { get; set; }
@@ -19,6 +23,6 @@ namespace food_delivery.Data.Models
         public string Email { get; set; }
 
         [ForeignKey("AddressId")]
-        public Guid AddressId { get; set; }
+        public Guid Addressid { get; set; }
     }
 }
