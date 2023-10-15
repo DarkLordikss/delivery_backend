aa8f2226dc2037c1205a74c850352f987924892b
Ivan Evchatov <ievchatov@inbox.ru>
2023-10-15 23:06:54 +0700

    Added HashPassword method

--- Services/UserService.cs
+++ Services/UserService.cs
@@ -25,5 +25,21 @@ namespace food_delivery.Services
             }
         }
 
+        private string HashPassword(string password, string salt)
+        {
+            byte[] saltBytes = Convert.FromBase64String(salt);
+            byte[] passwordBytes = Encoding.UTF8.GetBytes(password);
+            byte[] saltedPassword = new byte[saltBytes.Length + passwordBytes.Length];
+
+            Array.Copy(saltBytes, 0, saltedPassword, 0, saltBytes.Length);
+            Array.Copy(passwordBytes, 0, saltedPassword, saltBytes.Length, passwordBytes.Length);
+
+            using (var sha256 = SHA256.Create())
+            {
+                byte[] hashedPassword = sha256.ComputeHash(saltedPassword);
+                return Convert.ToBase64String(hashedPassword);
+            }
+        }
+
     }
 }
