8c3c9963672488729b727c490b57727e64f01377
Ivan Evchatov <ievchatov@inbox.ru>
2023-10-17 21:26:59 +0700

    Added GenerateToken

--- Services/TokenService.cs
+++ Services/TokenService.cs
@@ -1,16 +1,19 @@
 ﻿using System;
-using System.Linq;
-using Microsoft.EntityFrameworkCore;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
 using food_delivery.Data;
-using food_delivery.Data.Models;
+using food_delivery;
+using Microsoft.IdentityModel.Tokens;
 
 public class TokenService
 {
     private readonly AppDbContext _context;
+    private readonly SymmetricSecurityKey _securityKey;
 
     public TokenService(AppDbContext context)
     {
         _context = context;
+        _securityKey = AuthOptions.GetSymmetricSecurityKey();
     }
 
     public long? GetTokenSeriesByUserId(Guid userId)
@@ -19,5 +22,33 @@ public class TokenService
             .FirstOrDefault(p => p.UserId == userId);
 
         return userPassword?.TokenSeries;
+    }
+
+    public string? GenerateToken(Guid userId)
+    {
+        var credentials = new SigningCredentials(_securityKey, SecurityAlgorithms.HmacSha256);
+
+        var tokenSeries = GetTokenSeriesByUserId(userId);
+
+        if (tokenSeries == null)
+        {
+            return null;
+        }
+
+        var claims = new[]
+        {
+            new Claim(ClaimTypes.NameIdentifier, userId.ToString()),
+            new Claim(ClaimTypes.Version, tokenSeries.ToString()),
+        };
+
+        var token = new JwtSecurityToken(
+            issuer: AuthOptions.ISSUER,
+            audience: AuthOptions.AUDIENCE,
+            claims: claims,
+            expires: DateTime.UtcNow.AddMinutes(AuthOptions.LIFETIME),
+            signingCredentials: credentials
+        );
+
+        return new JwtSecurityTokenHandler().WriteToken(token);
     }
 }
