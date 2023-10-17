bb722fd253bb28772a786b9d5e85e44536d5bfbf
Ivan Evchatov <ievchatov@inbox.ru>
2023-10-17 17:21:13 +0700

    Added AuthOptions

---
+++ AuthOptions.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using System.Text;
+
+namespace food_delivery
+{
+    public class AuthOptions
+    {
+        public const string ISSUER = "BlanckProject";
+        public const string AUDIENCE = "DeliveryFrontend";
+        const string KEY = "fm2jf0923u1jJ!)((!@(@J193ej;askd-0oduwjqljasdsn";
+        public const int LIFETIME = 1;
+        public static SymmetricSecurityKey GetSymmetricSecurityKey()
+        {
+            return new SymmetricSecurityKey(Encoding.ASCII.GetBytes(KEY));
+        }
+    }
+}
