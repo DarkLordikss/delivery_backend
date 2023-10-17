f274fa80c1ec94a373a02d5d3aecb889c347ceeb
Ivan Evchatov <ievchatov@inbox.ru>
2023-10-17 18:18:41 +0700

    Added TokenSeriesRequirement

---
+++ Policies/TokenSeriesRequirement.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Authorization;
+
+namespace food_delivery.Policies
+{
+    public class TokenSeriesRequirement : IAuthorizationRequirement
+    {
+        public TokenSeriesRequirement(string tokenSeries)
+        {
+            TokenSeries = tokenSeries;
+        }
+
+        public string TokenSeries { get; }
+    }
+
+}
