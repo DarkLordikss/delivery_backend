2cd8ba8c1171f960038267fb2c37ff50a6e17641
Ivan Evchatov <ievchatov@inbox.ru>
2023-10-18 01:11:36 +0700

    Adde registration endpoint

---
+++ Controllers/UserController.cs
@@ -0,0 +1,32 @@
+﻿using food_delivery.Data.Models;
+using food_delivery.Services;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+
+namespace food_delivery.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly UserService _userService;
+        private readonly TokenService _tokenService;
+
+        public UserController(UserService userService, TokenService tokenService)
+        {
+            _userService = userService;
+            _tokenService = tokenService;
+        }
+
+        [HttpPost("register")]
+        public ActionResult Register(UserRegistrationModel user)
+        {
+            var userId = _userService.RegisterUser(user);
+
+            var token = _tokenService.GenerateToken(userId);
+
+            return Ok(new { Token = token});
+        }
+    }
+}
