66180d418bba29043ecbbf8a9860c55171a33ab5
Ivan Evchatov <ievchatov@inbox.ru>
2023-10-18 00:20:23 +0700

    Added UserRegistrationModel validation

--- Data/Models/UserRegistrationModel.cs
+++ Data/Models/UserRegistrationModel.cs
@@ -1,7 +1,34 @@
-﻿namespace food_delivery.Data.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace food_delivery.Data.Models
 {
-    public class UserRegistrationModel : User
+    public class UserRegistrationModel
     {
+        [Required(ErrorMessage = "Full name is required.")]
+        public string FullName { get; set; }
+
+        [Required(ErrorMessage = "Password is required.")]
+        [StringLength(30, MinimumLength = 8, ErrorMessage = "Password must be between 8 and 30 characters.")]
         public string Password { get; set; }
+
+        [Required(ErrorMessage = "Birth date is required.")]
+        [DataType(DataType.DateTime, ErrorMessage = "Birth date must be a valid date.")]
+        public DateTime BirthDate { get; set; }
+
+        [Required(ErrorMessage = "Gender is required.")]
+        [EnumDataType(typeof(Gender), ErrorMessage = "Invalid gender value.")]
+        public string Gender { get; set; }
+
+        [Required(ErrorMessage = "Phone is required.")]
+        [RegularExpression(@"^\+7 \(\d{3}\) \d{3}-\d{2}-\d{2}$", ErrorMessage = "Phone must match the format +7 (xxx) xxx-xx-xx.")]
+        public string Phone { get; set; }
+
+        [Required(ErrorMessage = "Email is required.")]
+        [EmailAddress(ErrorMessage = "Invalid email format.")]
+        public string Email { get; set; }
+
+        [ForeignKey("AddressId")]
+        public Guid Addressid { get; set; }
     }
 }
