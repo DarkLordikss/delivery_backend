0bc63b571888851d44bc77befac026203b3e3917
Ivan Evchatov <ievchatov@inbox.ru>
2023-10-17 18:10:18 +0700

    Added TokenService

---
+++ Services/TokenService.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+using food_delivery.Data;
+using food_delivery.Data.Models;
+
+public class TokenService
+{
+    private readonly AppDbContext _context;
+
+    public TokenService(AppDbContext context)
+    {
+        _context = context;
+    }
+
+    public long GetTokenSeriesByUserId(Guid userId)
+    {
+        var userPassword = _context.Passwords
+            .FirstOrDefault(p => p.UserId == userId);
+
+        return userPassword?.TokenSeries;
+    }
+}
