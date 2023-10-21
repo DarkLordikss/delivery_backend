4e6ae575b2ef163fec442dea54c62dbbe40b3811
Ivan Evchatov <ievchatov@inbox.ru>
2023-10-21 18:48:28 +0700

    Added BasketController

---
+++ Controllers/BasketController.cs
@@ -0,0 +1,23 @@
+﻿using food_delivery.ErrorModels;
+using food_delivery.ResponseModels;
+using food_delivery.Services;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Swashbuckle.AspNetCore.Annotations;
+using System.Security.Claims;
+
+namespace food_delivery.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BasketController : ControllerBase
+    {
+        private readonly BasketService _basketService;
+
+        public BasketController(BasketService basketService)
+        {
+            _basketService = basketService;
+        }
+
+    }
+}
