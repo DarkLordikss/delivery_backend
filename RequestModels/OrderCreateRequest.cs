5e30acc0df6849b517fd9bc1aeb96940781881c2
Ivan Evchatov <ievchatov@inbox.ru>
2023-10-22 16:10:34 +0700

    Added requirement of field for create order

--- RequestModels/OrderCreateRequest.cs
+++ RequestModels/OrderCreateRequest.cs
@@ -4,9 +4,11 @@ namespace food_delivery.RequestModels
 {
     public class OrderCreateRequest
     {
+        [Required(ErrorMessage = "DeliveryTime is required.")]
         [DataType(DataType.DateTime, ErrorMessage = "Birth date must be a valid date.")]
         public DateTime DeliveryTime { get; set; }
 
+        [Required(ErrorMessage = "AddressId is required.")]
         public Guid AddressId { get; set; }
     }
 }
