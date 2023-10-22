27fd2f4430cf8ff15fdddd4cbd9bc0b81a6bc123
Ivan Evchatov <ievchatov@inbox.ru>
2023-10-22 15:44:24 +0700

    Added time validation format for deliveryTime

--- RequestModels/OrderCreateRequest.cs
+++ RequestModels/OrderCreateRequest.cs
@@ -1,7 +1,10 @@
-﻿namespace food_delivery.RequestModels
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace food_delivery.RequestModels
 {
     public class OrderCreateRequest
     {
+        [DataType(DataType.DateTime, ErrorMessage = "Birth date must be a valid date.")]
         public DateTime DeliveryTime { get; set; }
 
         public Guid AddressId { get; set; }
