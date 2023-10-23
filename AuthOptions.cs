d009c33acca9dc1aaee5dd7a5920f688d9251aa0
Ivan Evchatov <ievchatov@inbox.ru>
2023-10-23 11:13:54 +0700

    Changed tokens lifetime from 1 to 5 minutes

--- AuthOptions.cs
+++ AuthOptions.cs
@@ -8,7 +8,7 @@ namespace food_delivery
         public const string ISSUER = "BlanckProject";
         public const string AUDIENCE = "DeliveryFrontend";
         const string KEY = "fm2jf0923u1jJ!)((!@(@J193ej;askd-0oduwjqljasdsn";
-        public const int LIFETIME = 1;
+        public const int LIFETIME = 5;
         public static SymmetricSecurityKey GetSymmetricSecurityKey()
         {
             return new SymmetricSecurityKey(Encoding.ASCII.GetBytes(KEY));
